150cb3837d3243aa6fa86ea8df54d6328ed2f7c3
JPaulooo <jpaulo.af3@gmail.com>
2026-04-26 17:26:59 -0300

    Update gerarDashboard.py

--- gerarDashboard.py
+++ gerarDashboard.py
@@ -13,7 +13,7 @@ st.title("🏥 Central de Monitoramento e Previsão de Deterioração")
 # =====================================================================
 @st.cache_data(ttl=60) 
 def carregar_dados():
-    file_id = '1WrkeWYzIPnIRVRX58UkJ07v43MChff_V'
+    file_id = '1WrkeWYzIPnIRVRX58UkJ07v43MChff'
     url_drive = f'https://drive.google.com/uc?id={file_id}&export=download'
     
     try:
